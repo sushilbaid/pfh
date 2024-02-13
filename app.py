80c7b3308925a301bedffef241f922605942f5e9
Sushil Baid <sushilbaid@gmail.com>
2024-02-13 12:02:44 +0530

    deployment script tbd

--- app.py
+++ app.py
@@ -5,6 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Web App with python Flask!'
-
-print(__name__)
-app.run(host='0.0.0.0', port=8000)
+# app.run(host='0.0.0.0', port=8000)
